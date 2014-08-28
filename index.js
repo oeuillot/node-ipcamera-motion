0a9af7cae07136cea8799c320e5c6b57c252b03c
oeuillot <olivier.oeuillot@gmail.com>
2014-08-28 17:33:24 +0200

    First version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var IPCamera = require('node-ipcamera');
 var http = require('http');
 var cv = require('opencv');
 var program = require('commander');
+var fs = require('fs');
 
 program.option("-u, --url <url>", "Mjpeg stream URL");
 
@@ -23,7 +24,49 @@ var d1 = new cv.Matrix();
 var d2 = new cv.Matrix();
 var motion = new cv.Matrix();
 
-function processImage(jpeg) {
+var frames = [];
+var recordTo = 0;
+var recordStream = 0;
+
+function processImage(jpeg, saved) {
+
+	var now = Date.now();
+
+	if (saved) {
+		// Nothing
+
+	} else if (recordStream && recordTo > now) {
+		console.error("record image");
+		recordStream.writeJpeg(jpeg, function(error) {
+			if (error) {
+				console.error(error);
+			}
+
+			processImage(jpeg, true);
+		});
+		return;
+
+	} else {
+		if (recordStream) {
+			console.error("Close stream");
+			recordTo = 0;
+			recordStream.close();
+			recordStream = null;
+		}
+
+		for (; frames.length;) {
+			if (frames[0].date.getTime() > now - 2000) {
+				break;
+			}
+
+			frames.unshift();
+		}
+
+		frames.push(jpeg);
+	}
+
+	console.error(frames.length + " images in memory");
+
 	cv.readImage(jpeg.data, function(err, mat) {
 		if (err) {
 			console.error(err);
@@ -50,22 +93,41 @@ function processImage(jpeg) {
 		var dev = motion.meanStdDev();
 		var stddev = dev.stddev.get(0, 0);
 		console.error("Deviation=", stddev);
-		if (stddev < 20) {
+		if (stddev < 10) {
 			multipartStream.once('jpeg', processImage);
 			return;
 		}
 
-		console.error("Saving ...");
+		recordTo = now + 2000;
 
-		motion.saveAsync('/tmp/img' + (cnt++) + '.jpg', function(error) {
+		console.error("Record to " + recordTo);
+
+		if (recordStream) {
+			multipartStream.once('jpeg', processImage);
+			return;
+		}
+
+		var fsStream = fs.createWriteStream('/tmp/img' + (cnt++) + '.mjpeg');
+		recordStream = new IPCamera.MultipartMjpegEncoderStream({}, fsStream);
+
+		var imgs = frames;
+		frames = [];
+
+		function writeNext(error) {
 			if (error) {
 				console.error(error);
+				imgs = [];
+			}
+			if (!imgs.length) {
+				multipartStream.once('jpeg', processImage);
+				return;
 			}
 
-			console.error("Motion saved");
-			multipartStream.once('jpeg', processImage);
+			recordStream.writeJpeg(imgs.shift(), writeNext);
+		}
 
-		});
+		writeNext();
+
 	});
 }
 
@@ -73,7 +135,7 @@ multipartStream.once('jpeg', processImage);
 
 var request = http.request(program.url, function(response) {
 
-	if (response.statusCode != 200) {
+	if (response.statusCode !== 200) {
 		throw new Error("Invalid status code of response " + response.statusCode);
 	}
 
