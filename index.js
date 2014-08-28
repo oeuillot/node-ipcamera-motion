58a60c7a85bbe52740fd69037bb837200ea44c13
oeuillot <olivier.oeuillot@gmail.com>
2014-08-28 23:11:50 +0200

    Add new parameters

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ program.option("-d, --detectionLevel <level>", "Detection level", parseFloat);
 
 program.option("-p, --storePath <path>", "Store path");
 
+program.option("--thresholdLevel <0..255>", "Image threshold", parseInt)
+
 program.parse(process.argv);
 
 if (!program.url) {
@@ -24,7 +26,8 @@ if (!program.storePath) {
 var multipartStream = new IPCamera.MultipartMjpegDecoderStream();
 
 var stddevLevel = program.detectionLevel || 5;
-var cnt = 0;
+var thresholdLevel = program.thresholdLevl || 35;
+var erodeIteration = 2;
 
 var prevFrame;
 var currentFrame;
@@ -38,6 +41,68 @@ var frames = [];
 var recordTo = 0;
 var recordStream = 0;
 var maxImages = 0;
+
+function createPath(basePath, date, prefix, suffix) {
+	var mn = date.getMonth() + 1;
+	var md = date.getDate();
+	var mh = date.getHours();
+	var mi = date.getMinutes();
+	var ms = date.getSeconds();
+
+	var py = path.join(basePath, String(date.getFullYear()));
+	try {
+		fs.statSync(py);
+	} catch (x) {
+		if (x.code === 'ENOENT') {
+			fs.mkdirSync(py);
+		} else {
+			console.log(x);
+			throw x;
+		}
+	}
+
+	var pm = path.join(py, ((mn < 10) ? "0" : "") + mn);
+	try {
+		fs.statSync(pm);
+	} catch (x) {
+		if (x.code === 'ENOENT') {
+			fs.mkdirSync(pm);
+		} else {
+			console.log(x);
+			throw x;
+		}
+	}
+
+	var pd = path.join(pm, ((md < 10) ? "0" : "") + md);
+	try {
+		fs.statSync(pd);
+	} catch (x) {
+		if (x.code === 'ENOENT') {
+			fs.mkdirSync(pd);
+		} else {
+			console.log(x);
+			throw x;
+		}
+	}
+
+	var ph = path.join(pd, ((mh < 10) ? "0" : "") + mh);
+	try {
+		fs.statSync(ph);
+	} catch (x) {
+		if (x.code === 'ENOENT') {
+			fs.mkdirSync(ph);
+		} else {
+			console.log(x);
+			throw x;
+		}
+	}
+
+	var p = path.join(ph, prefix + date.getFullYear() + "-" + ((mn < 10) ? "0" : "") + mn + "-" + ((md < 10) ? "0" : "") +
+			md + " " + ((mh < 10) ? "0" : "") + mh + "-" + ((mi < 10) ? "0" : "") + mi + "-" + ((ms < 10) ? "0" : "") + ms +
+			suffix);
+
+	return p;
+}
 
 function processImage(jpeg) {
 
@@ -91,12 +156,12 @@ function processImage(jpeg) {
 		d1.absDiff(prevFrame, nextFrame);
 		d2.absDiff(nextFrame, currentFrame);
 		motion.bitwiseAnd(d1, d2);
-		motion.threshold(35, 255);
-		motion.erode(1);
+		motion.threshold(thresholdLevel, 255);
+		motion.erode(erodeIteration);
 
 		var dev = motion.meanStdDev();
 		var stddev = dev.stddev.get(0, 0);
-		//console.error("Deviation=", stddev);
+		// console.error("Deviation=", stddev);
 		if (stddev < stddevLevel) {
 			multipartStream.once('jpeg', processImage);
 			return;
@@ -115,16 +180,7 @@ function processImage(jpeg) {
 			return;
 		}
 
-		var date = jpeg.date;
-		var mn = date.getMonth() + 1;
-		var md = date.getDate();
-		var mh = date.getHours();
-		var mi = date.getMinutes();
-		var ms = date.getSeconds();
-
-		var p = path.join(program.storePath, "Image " + date.getFullYear() + "-" + ((mn < 10) ? "0" : "") + mn + "-" +
-				((md < 10) ? "0" : "") + md + " " + ((mh < 10) ? "0" : "") + mh + "-" + ((mi < 10) ? "0" : "") + mi + "-" +
-				((ms < 10) ? "0" : "") + ms + ".mjpeg");
+		var p = createPath(program.storePath, jpeg.date, "Movie ", ".mjpeg");
 
 		var fsStream = fs.createWriteStream(p);
 		recordStream = new IPCamera.MultipartMjpegEncoderStream({}, fsStream);
