57dda86caf72ff2681b7aedb10627a24782a3c20
oeuillot <olivier.oeuillot@gmail.com>
2014-08-29 15:41:23 +0200

    new API structure

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var IPCamera = require('node-ipcamera');
+var API = require('./lib/API');
 var http = require('http');
-var cv = require('opencv');
 var program = require('commander');
-var fs = require('fs');
-var path = require('path');
+var IPCamera = require('node-ipcamera');
+var Events = require('events');
 
 program.option("-u, --url <url>", "Mjpeg stream URL");
 
@@ -27,196 +26,9 @@ if (!program.storePath) {
 	throw new Error("StorePath parameter must be specified");
 }
 
-var multipartStream = new IPCamera.MultipartMjpegDecoderStream();
+var lastJpegEventEmitter = new Events.EventEmitter();
 
-var stddevLevel = program.detectionLevel || 5;
-var thresholdLevel = program.thresholdLevl || 35;
-var erodeIteration = 2;
-
-var prevFrame;
-var currentFrame;
-var nextFrame;
-
-var firstImageDate = 0;
-var imageIndex = 0;
-var imageDelayMs = Math.floor(1000 / (program.detectionFPS || 25));
-
-var d1 = new cv.Matrix();
-var d2 = new cv.Matrix();
-var motion = new cv.Matrix();
-
-var frames = [];
-var recordTo = 0;
-var recordStream = 0;
-var maxImages = 0;
-
-function createPath(basePath, date, prefix, suffix) {
-	var mn = date.getMonth() + 1;
-	var md = date.getDate();
-	var mh = date.getHours();
-	var mi = date.getMinutes();
-	var ms = date.getSeconds();
-
-	var p = path.join(basePath, String(date.getFullYear()));
-	try {
-		fs.statSync(p);
-	} catch (x) {
-		if (x.code === 'ENOENT') {
-			fs.mkdirSync(p);
-		} else {
-			console.log(x);
-			throw x;
-		}
-	}
-
-	p = path.join(p, ((mn < 10) ? "0" : "") + mn);
-	try {
-		fs.statSync(p);
-	} catch (x) {
-		if (x.code === 'ENOENT') {
-			fs.mkdirSync(p);
-		} else {
-			console.log(x);
-			throw x;
-		}
-	}
-
-	p = path.join(p, ((md < 10) ? "0" : "") + md);
-	try {
-		fs.statSync(p);
-	} catch (x) {
-		if (x.code === 'ENOENT') {
-			fs.mkdirSync(p);
-		} else {
-			console.log(x);
-			throw x;
-		}
-	}
-
-	if (false) {
-		p = path.join(p, ((mh < 10) ? "0" : "") + mh);
-		try {
-			fs.statSync(p);
-		} catch (x) {
-			if (x.code === 'ENOENT') {
-				fs.mkdirSync(p);
-			} else {
-				console.log(x);
-				throw x;
-			}
-		}
-	}
-
-	p = path.join(p, prefix + date.getFullYear() + "-" + ((mn < 10) ? "0" : "") + mn + "-" + ((md < 10) ? "0" : "") + md +
-			" " + ((mh < 10) ? "0" : "") + mh + "-" + ((mi < 10) ? "0" : "") + mi + "-" + ((ms < 10) ? "0" : "") + ms +
-			suffix);
-
-	return p;
-}
-
-function processImage(jpeg) {
-
-	var now = Date.now();
-
-	if (recordStream && recordTo + 500 >= now) {
-		recordStream.pushJpeg(jpeg);
-
-	} else {
-		recordTo = 0;
-
-		for (; frames.length;) {
-			if (frames[0].date.getTime() > now - 2000) {
-				break;
-			}
-
-			if (recordStream) {
-				console.error("Close stream");
-				recordStream.close();
-				recordStream = null;
-			}
-
-			frames.shift();
-		}
-
-		frames.push(jpeg);
-	}
-
-	if (frames.length > maxImages) {
-		maxImages = frames.length;
-		console.error(maxImages + " images in memory");
-	}
-
-	if (firstImageDate + imageIndex * imageDelayMs > now) {
-		// console.error("Skip frame");
-
-		multipartStream.once('jpeg', processImage);
-		return;
-	}
-	imageIndex++;
-	// console.error("Process frame");
-
-	cv.readImage(jpeg.data, function(err, mat) {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		mat.convertGrayscale();
-
-		prevFrame = currentFrame;
-		currentFrame = nextFrame;
-		nextFrame = mat;
-
-		if (!prevFrame || !currentFrame || !nextFrame) {
-			multipartStream.once('jpeg', processImage);
-			return;
-		}
-
-		d1.absDiff(prevFrame, nextFrame);
-		d2.absDiff(nextFrame, currentFrame);
-		motion.bitwiseAnd(d1, d2);
-		motion.threshold(thresholdLevel, 255);
-		motion.erode(erodeIteration);
-
-		var dev = motion.meanStdDev();
-		var stddev = dev.stddev.get(0, 0);
-		if (program.showDeviation) {
-			var dsl = new Date();
-			process.stdout.write(dsl.toISOString() + " Deviation=" + stddev + "              \r");
-		}
-		if (stddev < stddevLevel) {
-			multipartStream.once('jpeg', processImage);
-			return;
-		}
-		if (program.showDeviation) {
-			process.stdout.write("\n");
-		}
-
-		recordTo = now + 2500;
-
-		if (recordStream) {
-			for (; frames.length;) {
-				recordStream.pushJpeg(frames.shift());
-			}
-
-			multipartStream.once('jpeg', processImage);
-			return;
-		}
-
-		var p = createPath(program.storePath, jpeg.date, "Movie ", ".mjpeg");
-
-		var fsStream = fs.createWriteStream(p);
-		recordStream = new IPCamera.MultipartMjpegEncoderStream({}, fsStream);
-
-		for (; frames.length;) {
-			recordStream.pushJpeg(frames.shift());
-		}
-
-		multipartStream.once('jpeg', processImage);
-	});
-}
-
-multipartStream.once('jpeg', processImage);
+var motionDetect = new API.MotionDetect(lastJpegEventEmitter);
 
 var request = http.request(program.url, function(response) {
 
@@ -224,7 +36,11 @@ var request = http.request(program.url, function(response) {
 		throw new Error("Invalid status code of response " + response.statusCode);
 	}
 
-	firstImageDate = Date.now();
+	var multipartStream = new IPCamera.MultipartMjpegDecoderStream();
+	multipartStream.on("jpeg", function(jpeg) {
+		lastJpegEventEmitter.emit("jpeg", jpeg);
+	});
+
 	response.pipe(multipartStream);
 });
 
